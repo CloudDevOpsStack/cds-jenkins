3a733968847a640b94496ae3afc0edb89f1cb1bf
PavanDevops <pavan@techolution.com>
2017-04-25 16:29:15 +0530

    Update install-plugins.sh

--- install-plugins.sh
+++ install-plugins.sh
@@ -2,11 +2,11 @@
 echo -ne "\nDoing initial setup\n "
 
 JENKINS_PLUGINS_URL=https://updates.jenkins-ci.org/latest/
-JENKINS_PLUGINS_DIR=$JENKINS_HOME/plugins
+JENKINS_PLUGINS_DIR=/opt/jenkins/plugins
 
 # Create plugins directory if it doesn't exist
 mkdir -p $JENKINS_PLUGINS_DIR
 
 while read p; do
-  wget -c -r -l0 --no-directories --ignore-case -A "$p.*" $JENKINS_PLUGINS_URL -P "$JENKINS_HOME/plugins"
+  wget -c -r -l0 --no-directories --ignore-case -A "$p.*" $JENKINS_PLUGINS_URL -P "/opt/jenkins/plugins"
 done < plugins.txt
